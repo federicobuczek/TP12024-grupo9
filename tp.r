a33baf0f14f3f48bd8e5a26ca266987002a215b2
nicolasmarinzalda <166338916+nicolasmarinzalda@users.noreply.github.com>
2024-04-11 18:02:48 -0300

    Add files via upload

--- tp.r
+++ tp.r
@@ -10,10 +10,13 @@ tabla <- read_xlsx("Datos_LP.xlsx",
 tabla_tiempo_de_residencia <- table(tabla$`Tiempo de residencia en la vivienda actual (en años)`)
 tabla_presion_de_agua <- table(tabla$`¿Cómo es la presión del agua?`)
 
-#
+####
 
-cantidad_entrevistados <- sum(tabla_presion_de_agua)
-frec_rel_presion_de_agua <- round(tabla_presion_de_agua / cantidad_entrevistados, 2)
+fuente <- "Fuente: El Observatorio Villero, La Poderosa. Relevamiento de Condiciones habitacionales 2022"
+colores1 <- c("#79155B", "#C23373", "#F6635C", "#FFBA86")
+colores2 <- c("#884A39", "#C38154", "#FFC26F", "#F9E0BB")
+cantidad_entrevistados <- 1222
+frec_rel_presion_de_agua <- round(tabla_presion_de_agua / cantidad_entrevistados, 4)
 
 
 # Boxplot para ver claramente la distribución
@@ -23,7 +26,7 @@ boxplot(tabla_tiempo_de_residencia,
         xlab = "Tiempo (en años)",
         main = "Tiempo de residencia en la vivienda actual.",
         col = "lightpink",
-        sub = "Fuente: El Observatorio Villero, La Poderosa. Relevamiento de Condiciones habitacionales 2022",
+        sub = fuente,
         cex.sub = 0.7)
 
 axis(side = 1,
@@ -37,10 +40,71 @@ axis(side = 1,
 labels_presion_de_agua <- paste(c("Buena", "Débil", "Muy débil"), "\n ", frec_rel_presion_de_agua * 100, "%")
 pie(tabla_presion_de_agua,
     labels = labels_presion_de_agua,
-    col = c("green3", "yellow", "red"),
+    col = colores2,
     main = "¿Cómo es la presión del agua?",
+    sub = fuente,
+    cex.sub = 0.9,
     clockwise = TRUE)
 
-mtext("Fuente: El Observatorio Villero, La Poderosa. \n Relevamiento de Condiciones habitacionales 2022",
-      side = 1, cex = 0.9, line = 2, font = 2)
+# Histograma integrantes por vivienda:
+
+integrantes_mediana <- paste("Mediana: ", median(tabla$`¿Cuántos integrantes hay en su vivienda?`))
+cuartiles_integrantes <- quantile(tabla$`¿Cuántos integrantes hay en su vivienda?`)
+rango_intercuartilico_integrantes <- paste("Rango intercuartílico: ", IQR(tabla$`¿Cuántos integrantes hay en su vivienda?`))
+cuartiles_integrantes <- paste("Cuartiles: (", cuartiles_integrantes[2], ",",
+                               cuartiles_integrantes[3], ",",
+                               cuartiles_integrantes[4], ")")
+
+
+      
+plot(table(tabla$`¿Cuántos integrantes hay en su vivienda?`),
+     type = 'h',
+     xlim = c(1,10),
+     xlab = "Integrantes por vivienda",
+     ylab = "Frecuencia absoluta",
+     main = "¿Cuántos integrantes hay en su vivienda?",
+     sub = fuente,
+     cex.sub = 0.7,
+     col = "black",
+     font.lab = 2,
+     frame.plot = FALSE)
+
+mtext(paste(integrantes_mediana, "\n", cuartiles_integrantes, "\n", rango_intercuartilico_integrantes),
+      side = 3,
+      cex = 0.7,
+      adj = 1,
+      line = -3,
+      font = 2)
+
+# Plagas
+tabla_plaga <- table(tabla$`¿Hay plagas (cucarachas, mosquitos, ratas, etc) en su vivienda y en los alrededores de la misma?`)
+porcentaje_plaga = paste(round(tabla_plaga / sum(tabla_plaga) * 100, 2), c('%'), sep = " ")
+categorias_plaga = c("Sí", "No")
+etiquetas_plaga = paste(categorias_plaga, porcentaje_plaga, sep = "\n")
+pie(tabla_plaga,
+    main = "¿Hay plagas en tu vivienda?",
+    labels = etiquetas_plaga,
+    cex = 0.8,
+    sub = fuente,
+    col = colores2,
+    cex.sub = 0.8
+)
+
+cucarachas = table(factor(tabla$`¿Cuáles plagas?`))
+mosquitos = table(factor(tabla$...94))
+ratas = table(factor(tabla$...95))
+tabla_plagas = data.frame(FrecuenciaAbsoluta = c(cucarachas, mosquitos, ratas))
+frec_rel_plagas = round(tabla_plagas$FrecuenciaAbsoluta / cantidad_entrevistados, 2)
+names(frec_rel_plagas) = c("cucarachas", "mosquitos", "ratas")
+frec_rel_plagas = frec_rel_plagas[order(frec_rel_plagas, decreasing = TRUE)]
+par(mar = c(5, 6, 4, 2) + 0.1)
+barplot(frec_rel_plagas,
+        horiz = TRUE,
+        xlim = c(0, 1),
+        xlab = "Frecuencia relativa",
+        main = "¿Cuales plagas?",
+        sub = fuente,
+        col = "lightblue",
+        las = 1,
+        cex.sub = 0.8)
 
